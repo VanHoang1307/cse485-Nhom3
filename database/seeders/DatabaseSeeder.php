5c605e17276eabbaa0480ac397615ffd0dc2d6b9
xiaohong1206 <thuyhuong12062k4@gmail.com>
2026-08-05 01:02:20 +0700

    Resolve merge conflict in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,43 +2,23 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-=======
->>>>>>> 8ee60937cae970876e6aa4aeb5da80c206e82860
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     use WithoutModelEvents;
 
-=======
->>>>>>> 8ee60937cae970876e6aa4aeb5da80c206e82860
-    /**
-     * Seed the application's database.
-     */
-=======
->>>>>>> c63bd8db062e2b0a26835be02a104358fb4812dd
     public function run(): void
     {
         $this->call([
-<<<<<<< HEAD
             StudentSeeder::class,
             ApplicationSeeder::class,
             ApplicationDocumentSeeder::class,
             EvaluationScoreSeeder::class,
             RankingResultSeeder::class,
-        ]);
-    }
-    
-}
-=======
             ScholarshipProgramSeeder::class,
             EligibilityRuleSeeder::class,
         ]);
     }
 }
->>>>>>> 8ee60937cae970876e6aa4aeb5da80c206e82860
